4c508a8b2dcaf8acb47f19c00297fc0f154e9a3e
johnnydolfo <126531124+johnnydolfo@users.noreply.github.com>
2024-07-08 17:13:59 -0300

    Add files via upload
    tela final de favoritos

--- favoritos.js
+++ favoritos.js
@@ -1,27 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const favoriteIcons = document.querySelectorAll('.favorite-icon');
     
-    const isFirstVisit = !Array.from(favoriteIcons).some(icon => localStorage.getItem(`favorite_${icon.dataset.restaurantId}`) !== null);
-
-    favoriteIcons.forEach(icon => {
-        const restaurantId = icon.dataset.restaurantId;
-
-        if (isFirstVisit) {
-            icon.dataset.favorite = "true";
-            icon.querySelector('img').src = 'imgs/redheart.png';
-            localStorage.setItem(`favorite_${restaurantId}`, JSON.stringify(true));
-        } else {
-            const isFavorite = JSON.parse(localStorage.getItem(`favorite_${restaurantId}`)) || false;
-            icon.dataset.favorite = isFavorite ? "true" : "false";
-            icon.querySelector('img').src = isFavorite ? 'imgs/redheart.png' : 'imgs/blankheart.png';
-        }
-    });
+    updateFavoriteRestaurants();
 
     const profileName = localStorage.getItem('profileName') || 'João';
     const profilePicture = localStorage.getItem('profilePicture') || 'imgs/user-icon.png';
     document.querySelector('.nome').textContent = profileName;
     document.querySelector('.profile-pic').setAttribute('src', profilePicture);
 });
+
+function updateFavoriteRestaurants() {
+    const favoriteIcons = document.querySelectorAll('.favorite-icon');
+    favoriteIcons.forEach(icon => {
+        const restaurantId = icon.dataset.restaurantId;
+        const isFavorite = JSON.parse(localStorage.getItem(`favorite_${restaurantId}`)) || false;
+        if (!isFavorite) {
+            icon.closest('.restaurant-card').remove();
+        } else {
+            icon.dataset.favorite = "true";
+            icon.querySelector('img').src = 'imgs/redheart.png';
+        }
+    });
+}
 
 function toggleFavorite(icon) {
     const restaurantId = icon.dataset.restaurantId;
