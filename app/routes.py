78f907478808bbeeab88683e8b0ab82cc3d51c69
lucas001x <lucascezararruda@gmail.com>
2024-08-06 17:22:02 -0300

    pasta atualizada

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,12 @@
-# app/routes.py
 from flask import Flask, render_template, request, redirect, url_for, flash
-from app.models import Deficiencia, User, Senha  # Importe as classes de modelo
+from app.models import Deficiencia, User, Senha
 from app import app, db
 
-@app.route('/')
+@app.route('/templates/')
 def index():
-    return render_template('static/index.html')
+    return render_template('index.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/template', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         nome = request.form['nome']
@@ -18,9 +17,10 @@ def signup():
 
         # Armazenamento no banco de dados
         novo_usuario = User(nome=nome, email=email, idade=idade, deficiencia=deficiencia_codigo)
-        nova_senha = Senha(id=novo_usuario.id, senha=senha)
-
         db.session.add(novo_usuario)
+        db.session.commit()
+
+        nova_senha = Senha(id=novo_usuario.id, senha=senha)
         db.session.add(nova_senha)
         db.session.commit()
 
@@ -30,21 +30,21 @@ def signup():
     deficiencias = Deficiencia.query.all()
     return render_template('signup.html', deficiencias=deficiencias)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha']
+# @app.route('/', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         senha = request.form['senha']
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            user_senha = Senha.query.filter_by(id=user.id, senha=senha).first()
-            if user_senha:
-                flash('Login realizado com sucesso!', 'success')
-                return redirect(url_for('index'))
-            else:
-                flash('Senha incorreta.', 'danger')
-        else:
-            flash('Usuário não encontrado.', 'danger')
+#         user = User.query.filter_by(email=email).first()
+#         if user:
+#             user_senha = Senha.query.filter_by(id=user.id, senha=senha).first()
+#             if user_senha:
+#                 flash('Login realizado com sucesso!', 'success')
+#                 return redirect(url_for('index'))
+#             else:
+#                 flash('Senha incorreta.', 'danger')
+#         else:
+#             flash('Usuário não encontrado.', 'danger')
 
-    return render_template('login.html')
+#     return render_template('login.html')
--- app/static/js/landing.js
+++ app/static/js/landing.js
@@ -15,7 +15,7 @@ function showSlide(index) {
 
 nextSlideBtn.addEventListener('click', () => {
     if (currentSlide === slides.length - 1) {
-        window.location.href = 'login/login.html';
+        window.location.href = 'templates/login.html';
     } else {
         currentSlide = (currentSlide + 1) % slides.length;
         showSlide(currentSlide);
@@ -23,7 +23,7 @@ nextSlideBtn.addEventListener('click', () => {
 });
 
 skipBtn.addEventListener('click', () => {
-    window.location.href = 'login/login.html';
+    window.location.href = 'templates/login.html';
 });
 
 showSlide(currentSlide);
@@ -51,7 +51,7 @@ function handleTouchMove(event) {
 
 function nextSlide() {
     if (currentSlide === slides.length - 1) {
-        window.location.href = 'login/login.html';
+        window.location.href = 'templates/login.html';
     } else {
         currentSlide = (currentSlide + 1) % slides.length;
         showSlide(currentSlide);
